67c4549d196a2cb8fa5aa6f05917978a2d318fec
hhendrix9 <hhendricks9@yahoo.com>
2023-11-13 09:42:56 -0500

    Add files via upload

---
+++ mindex-java-code-challenge/mindex-java-code-challenge/src/main/java/com/mindex/challenge/data/Compensation.java
@@ -0,0 +1,37 @@
+package com.mindex.challenge.data;
+
+public class Compensation {
+    private String employeeId;
+    private String salary;
+    private String effectiveDate;
+    
+    
+    
+    public String getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(String employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public String getSalary() {
+        return salary;
+    }
+
+    public void setSalary(String salary) {
+        this.salary = salary;
+    }
+
+    public String geteffectiveDate() {
+        return effectiveDate;
+    }
+    
+    public void seteffectiveDate(String effectiveDate) {
+        this.effectiveDate = effectiveDate;
+    }
+	
+	
+	
+	
+}
